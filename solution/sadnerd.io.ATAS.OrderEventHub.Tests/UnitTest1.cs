996a5a7bc16912ebed4a76d86ab6e16e36268180
sanderd <sanderd@sanderd.be>
2025-03-23 20:43:37 +0100

    Add test project

---
+++ solution/sadnerd.io.ATAS.OrderEventHub.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace sadnerd.io.ATAS.OrderEventHub.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
